1e5ee68996b0d23dbca03b9f1f5d8576163d32c9
Evgeniy Fedotov <hardpair@yandex.ru>
2024-12-01 14:24:32 +0300

    try multitrading

--- precode.go
+++ precode.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
@@ -14,12 +16,33 @@ import (
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
 	// 1. Функция Generator
 	// ...
+	defer close(ch)
+	var i int64 = 1
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			ch <- i
+			fn(i)
+			i++
+		}
+	}
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
 	// 2. Функция Worker
 	// ...
+	for {
+		v, ok := <-in
+		if !ok {
+			close(out)
+			break
+		}
+		out <- v
+		time.Sleep(time.Millisecond)
+	}
 }
 
 func main() {
@@ -27,6 +50,8 @@ func main() {
 
 	// 3. Создание контекста
 	// ...
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
@@ -34,8 +59,9 @@ func main() {
 
 	// генерируем числа, считая параллельно их количество и сумму
 	go Generator(ctx, chIn, func(i int64) {
-		inputSum += i
-		inputCount++
+		// Если предположить что геренатор распараллелят, то и в функции требуется проводить атомарные операции
+		atomic.AddInt64(&inputSum, i)   // вместо inputSum += i
+		atomic.AddInt64(&inputCount, 1) //inputCount++
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -56,6 +82,21 @@ func main() {
 
 	// 4. Собираем числа из каналов outs
 	// ...
+	// "синхронизируем", дожидаемся завершения работы сборщиков.
+	for i := 0; i < NumOut; i++ {
+		wg.Add(1)
+		go func(in_cn <-chan int64, cn int) {
+			defer wg.Done()
+			for {
+				v, ok := <-in_cn
+				if !ok {
+					break
+				}
+				amounts[cn]++
+				chOut <- v
+			}
+		}(outs[i], int(i))
+	}
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
@@ -69,6 +110,14 @@ func main() {
 
 	// 5. Читаем числа из результирующего канала
 	// ...
+	for {
+		v, ok := <-chOut
+		if !ok {
+			break
+		}
+		count++
+		sum += v
+	}
 
 	fmt.Println("Количество чисел", inputCount, count)
 	fmt.Println("Сумма чисел", inputSum, sum)
